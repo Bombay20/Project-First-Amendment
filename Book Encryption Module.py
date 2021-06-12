41734c75a39e7cfde8642096fb1f87af96fa9312
Bombay20 <85803575+Bombay20@users.noreply.github.com>
2021-06-12 19:27:38 +0100

    Add files via upload

---
+++ Book Encryption Module.py
@@ -0,0 +1,24 @@
+"""
+Project: First Amendment
+Author: Mr.X
+Date Created: 10/6/2021
+Date Last Modified: 10/6/2021
+"""
+"""
+import sqlite3
+def create_database(databasename,tablename):
+    conn = sqlite3.connect(databasename+'.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE (tablename)')
+
+create_database("fun","test")
+"""
+
+def bookcheck(book):#Checks the book file to make sure what char are advalable
+    
+
+
+
+def bookencryption():
+
+#TODO hello
